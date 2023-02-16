ece4f9a7ec1431837e15177a58788124d086aee0
William-Margareth <williammargarethpalllanjo@gmail.com>
2023-02-16 07:03:23 +0300

    String

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(){
+    var place = "akirachix"
+    println(place[0])
+    println(place[2])
+    println(place[3])
+}
+ fun greetings (name:String,age:Int):String{
+     var greet = "Hi,my name is $name and I am $age years old"
+     return greet
+ }
+fun source (name:String):Int{
+    var river = name.length
+    return river
+}
+fun word(){
+    var name = "Joyhermosa"
+    val word = "Thats me"
+    if(name==word) {
+    }else{
+        println("I don`t know who that is")
+    }
+}
+
