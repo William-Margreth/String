5e14ae3c0097c206d684040c9eb4a231f4695a8c
William-Margareth <williammargarethpalllanjo@gmail.com>
2023-02-16 08:30:43 +0300

    made changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,31 @@
 fun main(){
+    placename()
+    println(details("Joyhermosa",22))
+    source("")
+    nameCheck("Joy")
+
+
+
+}
+fun placename(){
     var place = "akirachix"
     println(place[0])
     println(place[2])
     println(place[3])
 }
- fun greetings (name:String,age:Int):String{
-     var greet = "Hi,my name is $name and I am $age years old"
-     return greet
- }
-fun source (name:String):Int{
-    var river = name.length
-    return river
+fun details(name:String,age :Int):String{
+    var details = "Hi, my name is $name and I am $age years old"
+    return details
 }
-fun word(){
-    var name = "Joyhermosa"
-    val word = "Thats me"
-    if(name==word) {
-    }else{
-        println("I don`t know who that is")
+
+fun source(name: String):Int{
+    var word = "river"
+    return word.length
+}
+fun nameCheck(name: String){
+    if (name=="Williams"){
+        println("Thats me!")}
+    else{
+        println("I don`t know who that is")}
     }
-}
 
